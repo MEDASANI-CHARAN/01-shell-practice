9193c5c857b1ff59c56a5a963280739e84df14bf
Medasani Charan <charanchinna646@gmail.com>
2025-08-27 19:36:51 +0530

    hello world started

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-
-
 TIMESTAMP=$(date)
 echo "Script executed at: $TIMESTAMP"
+
+# this is comment
+NUMBER1=100
+NUMBER2=200
+
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
