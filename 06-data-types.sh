c138afb9e0c7003f67588f503ff66ebd7a0deb96
Medasani Charan <charanchinna646@gmail.com>
2025-09-30 13:13:31 +0530

    shell-practice

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -6,9 +6,9 @@ echo "Script executed at: $TIMESTAMP"
 # we keep the command in $() and will take output as variable
 
 # this is comment
-NUMBER1=100
-NUMBER2=200
+# NUMBER1=100
+# NUMBER2=200
 
-SUM=$(($NUMBER1+$NUMBER2))
+# SUM=$(($NUMBER1+$NUMBER2))
 
-echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
+# echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
