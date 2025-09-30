b9db96ee19266f2d02ac04d75e14eb3d79504f8d
Medasani Charan <charanchinna646@gmail.com>
2025-09-30 22:33:21 +0530

    shell-practice

--- 07-array.sh
+++ 07-array.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-MOVIES=("Court" "HIT3" "PUSHPA2" "Thandel")
+MOVIES=("Court" "HIT3" "PUSHPA2" "Thandel") # index will start from "0"
 
 echo "First Movie: ${MOVIES[0]}"
+echo "First Movie: ${MOVIES[2]}"
 echo "First Movie: ${MOVIES[3]}"
-echo "First Movie: ${MOVIES[4]}"
 
 echo "All movies: ${MOVIES[@]}"
---
+++ 08-special-vars.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "All variables: $@"
+# echo "Number of varibles passed: $#"
+# echo "Name of the script: $0"
+# echo "Current working directoty: $PWD"
+# echo "Home directory of current user: $Home"
+# echo "Which user is runmning the script: $USER"
+# echo "Host name: $HOSTNAME"
+# echo "Process ID of current shell script: $$"
+# echo "Process ID of last background command: $!"
