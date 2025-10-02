9abb84f47d3e9e64bf821389a763145e4db57deb
Medasani Charan <charanchinna646@gmail.com>
2025-10-02 06:48:49 +0530

    shell-practice

---
+++ 12-colors.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo -e "\e[31m Hello colors \e[0m"
+echo -e "\e[32m Hello colors \e[0m"
+echo -e "\e[33m Hello colors \e[0m"
+
+echo "Hello No colors" # If we not end this the latest line of color applicable to this. we have to end every line with \e[0m
+
+# 31 -> Red
+# 32 -> Green
+# 33 -> Yellow
