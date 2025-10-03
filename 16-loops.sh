5770c0686090c68629f968267564fc42adef387e
Medasani Charan <charanchinna646@gmail.com>
2025-10-03 07:19:21 +0530

    shell-practice

--- 16-loops.sh
+++ 16-loops.sh
@@ -35,7 +35,7 @@ VALIDATE(){
 
 for package in ${PACKAGES[@]}
 do
-    dnf list installed mysql &>>$LOG_FILE
+    dnf list installed $package &>>$LOG_FILE
     if [ $? -ne 0 ]
     then
         echo "$package is not installed... going to install" | tee -a $LOG_FILE
