163e3f0dcde27ddbf35d0a348388e76e5bb7c89f
Medasani Charan <charanchinna646@gmail.com>
2025-10-12 07:45:08 +0530

    shell-practice

--- 17-set.sh
+++ 17-set.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 set -e
+failre(){
+    echo "Failed at: $1 $2"
+}
+trap 'failure "${LINENO}" "${BASH_COMMAND}"' ERR
 
 echo "Hi, Good Morning"
+echo "Hello, am learning DEvOps"
 echooo "Hello, this will be error"
 echo "Hello, How are you"
